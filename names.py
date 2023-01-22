df79a48ef95c7d336ed667bf38e7bc3d51f757cc
Izabela13 <izabela.jajkiewicz@gmail.com>
2023-01-22 16:31:50 +0100

    Dodano imię do funkcji print() i narysowano wzór.

--- names.py
+++ names.py
@@ -1,2 +1,18 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
+print("Mam na imię Iza")  # tutaj wpisz swoje imię
 # i dodaj jeszcze jakiś ciekawy kod
+
+print("Narysuję fajny wzorek za pomocą pętli for:")
+
+for x in ["*"]:
+    print(x, x, sep = " " * 8)
+    print(x * 2, x * 2, sep = " " * 6)
+    print(x * 3, x * 3, sep = " " * 4)
+    print(x * 4, x * 4, sep = " " * 2)
+    print(x * 5, x * 5, sep = " " * 0)
+    print(x * 4, x * 4, sep = " " * 2)
+    print(x * 3, x * 3, sep = " " * 4)
+    print(x * 2, x * 2, sep = " " * 6)
+    print(x, x, sep = " " * 8)
+
+print()
+print("Pozdrawiam.")
