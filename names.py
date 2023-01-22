86181c85a3084b0fbd206695c48c30dc4e0575a3
wojanus <wojanus@student.wszib.edu.pl>
2023-01-22 16:35:16 +0100

    Zapisałem zmiany

--- names.py
+++ names.py
@@ -1,2 +1,10 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
-# i dodaj jeszcze jakiś ciekawy kod
+print("Mam na imię Wojtek")
+
+# Obliczenia
+a=2
+print("Pole jest równe:", a**2)
+print("Obwód wynosi:", 4*a)
+print("Długość przekątnej kwadratu:", round(a*(2**0.5),2))
+print("\n")
+
+
