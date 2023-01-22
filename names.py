737337bad0205f16b4c7faaea7009915b3bb10c9
pikoloda <pikoloda@student.wszib.edu.pl>
2023-01-22 16:31:42 +0100

    Moje małe plany na następne lata :)

--- names.py
+++ names.py
@@ -1,2 +1,7 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
+print("Mam na imię Piotr")  # tutaj wpisz swoje imię
+print("Myśle że przed 50 zakoduje coś ciekawego :) ")
+print("Puki co lecimy ze zwykłą pętlą")
+for i in range(10):
+    print("Python 3.10 !")
 # i dodaj jeszcze jakiś ciekawy kod
+print("Udoskonalam pętle")
