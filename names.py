6cc6689b0544c6d19bcdbbd4f22894525348debb
pdyrda <pdyrda@student.wszib.edu.pl>
2023-01-22 16:21:43 +0100

    names.py - zmieniono pierwszy wiersz oraz dodano swój kod

--- names.py
+++ names.py
@@ -1,2 +1,13 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
+print("Mam na imię Przemysław")  # tutaj wpisz swoje imię
 # i dodaj jeszcze jakiś ciekawy kod
+
+import random
+
+numbers = []
+dice = 12
+times = 10
+for i in range(times):
+    number = random.randint(1, dice)
+    numbers.append(number)
+
+print(times, "rzutów kostką o", dice, "ściankach wynosi:", numbers)
