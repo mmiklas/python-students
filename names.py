08114f7b09a2108746635cf413e0798698c2d460
jnadach <jan.nadachowski@gmail.com>
2023-02-18 16:58:00 +0100

    Zadanie domowe

--- names.py
+++ names.py
@@ -1,2 +1,2 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
-# i dodaj jeszcze jakiś ciekawy kod
+print("Mam na imię Jan")  # tutaj wpisz swoje imię
+print("A to jest moje zadanie domowe z systemu kontroli wersji git")
