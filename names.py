9adee4973c5cb1f3017e5378dd2946fbb22822b6
MFurgala <furgala.mat@gmail.com>
2023-11-26 12:30:21 +0100

    Dodanie print trapezu

--- names.py
+++ names.py
@@ -1,2 +1,15 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
-# i dodaj jeszcze jakiś ciekawy kod
+print("Mam na imię Mateusz i narysuję trapez.")  # tutaj wpisz swoje imię
+print("***")
+print("*  *")
+print("*   *")
+print("*    *")
+print("*     *")
+print("*      *")
+print("*       *")
+print("*        *")
+print("*         *")
+print("*          *")
+print("*           *")
+print("*            *")
+print("*             *")
+print("****************")
