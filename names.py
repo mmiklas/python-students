93a079c44a0ced4f4077fd9e8bd206eecd095d93
adglowac <adglowac@student.wszib.edu.pl>
2023-11-26 12:33:14 +0100

    -zmiana imienia -dodanie generatora liczb

--- names.py
+++ names.py
@@ -1,2 +1,11 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
+print("Mam na imię Adrian.")  # tutaj wpisz swoje imię
+print("Wygenerowałem poniżej jeszcze 10 losowych liczb.")
+import random
+
+numbers = []
+
+for i in range(10):
+    numbers.append(int(random.randint(1, 10)))
+
+print(numbers)
 # i dodaj jeszcze jakiś ciekawy kod
